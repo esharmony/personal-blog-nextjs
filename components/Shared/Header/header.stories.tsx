d6b3fa0036e70db9c0a5a2f4795b58784f80a619
Matthew Barnden <development.esharmony@gmail.com>
2021-03-09 15:46:11 +0000

    posts done rtest push to storybook

--- components/Shared/Header/header.stories.tsx
+++ components/Shared/Header/header.stories.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { withNextRouter } from 'storybook-addon-next-router';
-
-import Header, { HeaderProps } from '.';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { graphql, SetupWorkerApi } from 'msw';
+import Header from '.';
+import { worker } from '../../../mocks/browser';
 
 export default {
   title: 'Blog/Components/Shared/Header',
@@ -11,7 +13,39 @@ export default {
   decorators: [withNextRouter],
 } as Meta;
 
-const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+const mockedQueryClientHeader = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const mockedQueryClientHeaderError = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const mockedQueryClientHeaderLoading = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const navigations = [
+  { Item: 'About Me', Slug: 'About-Me' },
+  { Item: 'Vlogs', Slug: 'Vlogs' },
+  { Item: 'Tutorials', Slug: 'Tutorials' },
+  { Item: 'Quick Tips', Slug: 'Quick-Tips' },
+  { Item: 'Process', Slug: 'Process' },
+];
+
+const Template: Story = () => <Header />;
 
 export const Full = Template.bind({});
 Full.parameters = {
@@ -20,15 +54,24 @@ Full.parameters = {
     asPath: '/',
   },
 };
-Full.args = {
-  Items: [
-    { Item: 'About Me', Slug: 'About-Me' },
-    { Item: 'Vlogs', Slug: 'Vlogs' },
-    { Item: 'Tutorials', Slug: 'Tutorials' },
-    { Item: 'Quick Tips', Slug: 'Quick-Tips' },
-    { Item: 'Process', Slug: 'Process' },
-  ],
-};
+Full.decorators = [
+  (Story: Story) => {
+    (worker as SetupWorkerApi).use(
+      graphql.query('Navigation', (req, res, ctx) => {
+        return res(
+          ctx.data({
+            navigations,
+          })
+        );
+      })
+    );
+    return (
+      <QueryClientProvider client={mockedQueryClientHeader}>
+        <Story />
+      </QueryClientProvider>
+    );
+  },
+];
 
 export const Mobile = Template.bind({});
 Mobile.parameters = {
@@ -40,12 +83,68 @@ Mobile.parameters = {
     asPath: '/',
   },
 };
-Mobile.args = {
-  Items: [
-    { Item: 'About Me', Slug: 'About-Me' },
-    { Item: 'Vlogs', Slug: 'Vlogs' },
-    { Item: 'Tutorials', Slug: 'Tutorials' },
-    { Item: 'Quick Tips', Slug: 'Quick-Tips' },
-    { Item: 'Process', Slug: 'Process' },
-  ],
+
+Mobile.decorators = [
+  (Story: Story) => {
+    (worker as SetupWorkerApi).use(
+      graphql.query('Navigation', (req, res, ctx) => {
+        return res(
+          ctx.data({
+            navigations,
+          })
+        );
+      })
+    );
+    return (
+      <QueryClientProvider client={mockedQueryClientHeader}>
+        <Story />
+      </QueryClientProvider>
+    );
+  },
+];
+
+export const Loading = Template.bind({});
+Loading.parameters = {
+  nextRouter: {
+    pathname: '/',
+    asPath: '/',
+  },
 };
+
+Loading.decorators = [
+  (Story: Story) => {
+    (worker as SetupWorkerApi).use(
+      graphql.query('Navigation', (req, res, ctx) => {
+        return res(ctx.delay('infinite'));
+      })
+    );
+    return (
+      <QueryClientProvider client={mockedQueryClientHeaderLoading}>
+        <Story />
+      </QueryClientProvider>
+    );
+  },
+];
+
+export const Error = Template.bind({});
+Error.parameters = {
+  nextRouter: {
+    pathname: '/',
+    asPath: '/',
+  },
+};
+
+Error.decorators = [
+  (Story: Story) => {
+    (worker as SetupWorkerApi).use(
+      graphql.query('Navigation', (req, res, ctx) => {
+        return res.networkError('Boom there was error');
+      })
+    );
+    return (
+      <QueryClientProvider client={mockedQueryClientHeaderError}>
+        <Story />
+      </QueryClientProvider>
+    );
+  },
+];
