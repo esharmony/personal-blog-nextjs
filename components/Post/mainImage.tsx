71e0b794ec8e36041dd7a9a7a6b50a0c05d0451b
Matthew Barnden <development.esharmony@gmail.com>
2021-03-17 19:22:32 +0000

    changed images to use next/image for optimsation

--- components/Post/mainImage.tsx
+++ components/Post/mainImage.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export interface MainImageProps {
   Url: string;
   Title: string;
@@ -7,21 +9,36 @@ export interface MainImageProps {
 const MainImage = (props: MainImageProps): JSX.Element => {
   return (
     <>
-      <img
-        src={props.Url}
-        alt={props.Title}
-        className={`max-w-sm hidden  md:float-right ml-10 mb-10 ${
+      <div
+        className={` w-max hidden md:float-right ml-10 mb-10 ${
           props.IsPreview ? 'md:hidden' : 'md:block'
         }`}
-      />
+      >
+        <Image
+          layout='fixed'
+          src={props.Url}
+          alt={props.Title}
+          width={320}
+          height={225}
+          quality={90}
+        />
+      </div>
+
       <div
         title={props.Title}
-        className={`h-24 mb-5 w-full bg-cover ${
+        className={`h-24 mb-5 w-full bg-cover overflow-hidden ${
           props.IsPreview ? 'md:block' : 'md:hidden'
         }`}
-        style={{ backgroundImage: `url(${props.Url})` }}
       >
-        &nbsp;
+        <Image
+          layout='responsive'
+          src={props.Url}
+          alt={props.Title}
+          width={320}
+          height={225}
+          quality={90}
+          className='image -top-11'
+        />
       </div>
     </>
   );
