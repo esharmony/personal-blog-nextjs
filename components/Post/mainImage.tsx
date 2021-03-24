a26403d9013fc78f6bb9150a9f745a75b2a34886
Matthew Barnden <development.esharmony@gmail.com>
2021-03-24 10:38:34 +0000

    after rebuild of chromatic

--- components/Post/mainImage.tsx
+++ components/Post/mainImage.tsx
@@ -1,56 +1,28 @@
-import Image, { ImageLoader } from 'next/image';
-
 export interface MainImageProps {
   Url: string;
   Title: string;
   IsPreview?: boolean;
 }
-
-interface ImageLoaderProps {
-  src: string;
-  width:string;
-  quality: number
-}
-
-
-const MainImage = (props: MainImageProps): JSX.Element => {
- 
-
+const MainImage = ({ Url, Title, IsPreview }: MainImageProps): JSX.Element => {
   return (
     <>
-      <div
-        className={`w-max hidden md:float-right ml-10 mb-10 ${
-          props.IsPreview ? 'md:hidden' : 'md:block'
+      <img
+        src={Url}
+        alt={Title}
+        className={`max-w-sm hidden  md:float-right ml-10 mb-10 ${
+          IsPreview ? 'md:hidden' : 'md:block'
         }`}
-      >
-        <Image
-          layout='fixed'
-          src={props.Url}
-          alt={props.Title}
-          width={320}
-          height={225}
-          quality={90}
-        />
-      </div>
-
+      />
       <div
-        title={props.Title}
-        className={`h-24 mb-5 w-full bg-cover overflow-hidden ${
-          props.IsPreview ? 'md:block' : 'md:hidden'
+        title={Title}
+        className={`h-24 mb-5 w-full bg-cover ${
+          IsPreview ? 'md:block' : 'md:hidden'
         }`}
+        style={{ backgroundImage: `url(${Url})`, backgroundPositionY:'-75px' }}
       >
-        <Image
-          layout='responsive'
-          src={props.Url}
-          alt={props.Title}
-          width={320}
-          height={225}
-          quality={90}
-          className='image'
-        />
+        &nbsp;
       </div>
     </>
   );
 };
-
 export default MainImage;
