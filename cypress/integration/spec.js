919bf052afb365fc8320800c3eaeee442be03530
Matthew Barnden <development.esharmony@gmail.com>
2021-04-01 17:02:36 +0100

    fixed test added command

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -8,8 +8,8 @@ describe('homepage', () => {
       .first()
       .then(($a) => {
         const txt = $a.text();
-        txt.trim();
-        const slug = txt.replace(/\s/g, '-');
+        const trimmed = txt.trim();
+        const slug = trimmed.replace(/\s/g, '-');
         cy.get('a').first().click();
         cy.location('pathname').should('eq', `/posts/${slug}`);
       });
@@ -24,8 +24,8 @@ describe('homepage', () => {
       .then(($a) => {
         cy.get($a.parent().siblings('h1')).then(($h1) => {
           const txt = $h1.text();
-          txt.trim();
-          const slug = txt.replace(/\s/g, '-');
+          const trimmed = txt.trim();
+          const slug = trimmed.replace(/\s/g, '-');
           cy.get('a').contains('read').first().click();
           cy.location('pathname').should('eq', `/post/${slug}`);
         });
